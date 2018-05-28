edf1294070f5de5a5fedc668933f1299fe8f600a
Ariel Zhen <zhenpingping@live.com>
2018-05-28 07:08:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,7 @@ def prime?(num)
   if num == 1 || num == -1 || num == 0
     return false
   else 
-  array = Array(2..num.abs-1)
-  array.all? {|x| num % x != 0}
+    array = Array(2..num.abs-1)
+    array.all? {|x| num % x != 0}
+  end 
 end
